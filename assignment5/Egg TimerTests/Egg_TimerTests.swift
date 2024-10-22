a78d604360f2a39b30005b58c4b69a03e42872ed
araibayaly <72352731+araibayaly@users.noreply.github.com>
2024-10-22 23:13:14 +0500

    Add files via upload

---
+++ assignment5/Egg TimerTests/Egg_TimerTests.swift
@@ -0,0 +1,11 @@
+
+import Testing
+@testable import Egg_Timer
+
+struct Egg_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
